52bdfdb261d8c3369c38e777143771518fa9e179
Braden Alsford <braden.alsford@myob.com>
2020-03-30 15:26:31 +1100

    Adds refactored Rule tests.

--- GameOfLife/GameOfLifeTests/RuleTests.cs
+++ GameOfLife/GameOfLifeTests/RuleTests.cs
@@ -1,87 +1,119 @@
-// using System.Collections.Generic;
-// using System.Linq;
-// using GameOfLife;
-// using GameOfLife.Business;
-// using GameOfLife.Business.Cell;
-// using GameOfLife.Business.Exceptions;
-// using GameOfLife.Business.Requirement;
-// using NUnit.Framework;
-// using Rule = GameOfLife.Business.Rule;
-//
-// namespace GameOfLifeTests
-// {
-//     public class RuleTests
-//     {
-//
-//         private Rule _overcrowdingRule;
-//         private List<List<Cell>> _grid = new List<List<Cell>>();
-//         
-//         [SetUp]
-//         public void Setup()
-//         {
-//             var cellAliveRequirement = new InitialStateRequirement(new HashSet<CellState>{CellState.Alive});
-//             var moreThanThreeNeighboursRequirement = new ActiveNeighbourRequirement(new HashSet<int>{4, 5, 6, 7, 8});
-//             var requirements = new List<IRequirement> {cellAliveRequirement, moreThanThreeNeighboursRequirement};
-//             
-//             _overcrowdingRule = new Rule(requirements, CellState.Dead);
-//
-//             for (var i = 0; i < 3; i++)
-//             {
-//                 _grid.Add(new List<Cell> {new Cell(), new Cell(), new Cell()});
-//             }
-//             
-//         }
-//
-//
-//         [Test]
-//         public void It_Should_Return_Dead_Given_OvercrowdingRule_With_Alive_Overcrowded_Cell()
-//         {
-//             foreach (var cell in _grid.SelectMany(row => row))
-//             {
-//                 cell.Revive();
-//             }
-//
-//
-//             Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(_grid));
-//         }
-//         
-//         [Test]
-//         public void It_Should_Return_Dead_Given_OvercrowdingRule_With_Dead_Overcrowded_Cell()
-//         {
-//             foreach (var cell in _grid.SelectMany(row => row))
-//             {
-//                 cell.Revive();
-//             }
-//             _grid[1][1].Kill();
-//             
-//             Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(_grid));
-//         }
-//         
-//         [Test]
-//         public void It_Should_Return_Dead_Given_OvercrowdingRule_With_Dead_Undercrowded_Cell()
-//         {
-//             foreach (var cell in _grid.SelectMany(row => row))
-//             {
-//                 cell.Kill();
-//             }
-//             _grid[0][0].Revive();
-//             _grid[0][1].Revive();
-//             
-//             Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(_grid));
-//         }
-//         
-//         [Test]
-//         public void It_Should_Return_Alive_Given_OvercrowdingRule_With_Alive_Undercrowded_Cell()
-//         {
-//             foreach (var cell in _grid.SelectMany(row => row))
-//             {
-//                 cell.Kill();
-//             }
-//             _grid[0][0].Revive();
-//             _grid[0][1].Revive();
-//             _grid[1][1].Revive();
-//             
-//             Assert.AreEqual(CellState.Alive, _overcrowdingRule.GetNextCellState(_grid));
-//         }
-//     }
-// }
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using GameOfLife.Business;
+using GameOfLife.Business.Cell;
+using GameOfLife.Business.Requirement;
+using NUnit.Framework;
+using Rule = GameOfLife.Business.Rule;
+
+namespace GameOfLifeTests
+{
+    public class RuleTests
+    {
+
+        private Rule _overcrowdingRule;
+
+        private List<ReadOnlyCell> _allAliveRow;
+        private List<ReadOnlyCell> _allDeadRow;
+
+        [SetUp]
+        public void Setup()
+        {
+            var cellAliveRequirement = new InitialStateRequirement(new HashSet<CellState>{CellState.Alive});
+            var moreThanThreeNeighboursRequirement = new ActiveNeighbourRequirement(new HashSet<int>{4, 5, 6, 7, 8});
+            var requirements = new List<IRequirement> {cellAliveRequirement, moreThanThreeNeighboursRequirement};
+            
+            _overcrowdingRule = new Rule(requirements, CellState.Dead);
+            
+            _allAliveRow = new List<ReadOnlyCell>
+            {
+                new ReadOnlyCell(CellState.Alive),
+                new ReadOnlyCell(CellState.Alive),
+                new ReadOnlyCell(CellState.Alive)
+            };
+            
+            _allDeadRow = new List<ReadOnlyCell>
+            {
+                new ReadOnlyCell(CellState.Dead),
+                new ReadOnlyCell(CellState.Dead),
+                new ReadOnlyCell(CellState.Dead)
+            };   
+            
+        }
+
+        [Test]
+        public void It_Should_Return_Dead_Given_An_Alive_Cell_With_Eight_Alive_Neighbours()
+        {
+            var allAliveGrid = new List<ReadOnlyCollection<ReadOnlyCell>>
+            {
+                new ReadOnlyCollection<ReadOnlyCell>(_allAliveRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allAliveRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allAliveRow)
+            };
+            
+            var allAliveReadOnlyGrid = new ReadOnlyCollection<ReadOnlyCollection<ReadOnlyCell>>(allAliveGrid);
+
+            Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(allAliveReadOnlyGrid));
+        }
+
+        [Test]
+        public void It_Should_Return_Dead_Given_An_Dead_Cell_With_Eight_Alive_Neighbours()
+        {
+            var deadCenterRow = new List<ReadOnlyCell>
+            {
+                new ReadOnlyCell(CellState.Alive),
+                new ReadOnlyCell(CellState.Dead),
+                new ReadOnlyCell(CellState.Alive)
+            };
+            
+            var deadCenterGrid = new List<ReadOnlyCollection<ReadOnlyCell>>
+            {
+                new ReadOnlyCollection<ReadOnlyCell>(_allAliveRow),
+                new ReadOnlyCollection<ReadOnlyCell>(deadCenterRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allAliveRow)
+            };
+            
+            var deadCenterReadOnlyGrid = new ReadOnlyCollection<ReadOnlyCollection<ReadOnlyCell>>(deadCenterGrid);
+
+            Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(deadCenterReadOnlyGrid));
+        }
+
+        [Test]
+        public void It_Should_Return_Alive_Given_An_Alive_Cell_With_No_Alive_Neighbours()
+        {
+            var aliveCenterRow = new List<ReadOnlyCell>
+            {
+                new ReadOnlyCell(CellState.Dead),
+                new ReadOnlyCell(CellState.Alive),
+                new ReadOnlyCell(CellState.Dead)
+            };
+            
+            var aliveCenterGrid = new List<ReadOnlyCollection<ReadOnlyCell>>
+            {
+                new ReadOnlyCollection<ReadOnlyCell>(_allDeadRow),
+                new ReadOnlyCollection<ReadOnlyCell>(aliveCenterRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allDeadRow)
+            };
+            
+            var aliveCenterReadOnlyGrid = new ReadOnlyCollection<ReadOnlyCollection<ReadOnlyCell>>(aliveCenterGrid);
+
+            Assert.AreEqual(CellState.Alive, _overcrowdingRule.GetNextCellState(aliveCenterReadOnlyGrid));
+        }
+        
+        [Test]
+        public void It_Should_Return_Dead_Given_A_Dead_Cell_With_No_Alive_Neighbours()
+        {
+            var allDeadGrid = new List<ReadOnlyCollection<ReadOnlyCell>>
+            {
+                new ReadOnlyCollection<ReadOnlyCell>(_allDeadRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allDeadRow),
+                new ReadOnlyCollection<ReadOnlyCell>(_allDeadRow)
+            };
+            
+            var allDeadReadOnlyGrid = new ReadOnlyCollection<ReadOnlyCollection<ReadOnlyCell>>(allDeadGrid);
+        
+            Assert.AreEqual(CellState.Dead, _overcrowdingRule.GetNextCellState(allDeadReadOnlyGrid));
+        }
+        
+    }
+}
