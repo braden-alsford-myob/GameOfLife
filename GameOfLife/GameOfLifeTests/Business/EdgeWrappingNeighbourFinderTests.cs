a4a1d97e3699d8b16de894e65efaa72dca4c8e9d
Braden Alsford <braden.alsford@myob.com>
2020-04-16 13:05:11 +1000

    Refactors edgewrapping neighbour finder tests to use paramaterized test cases.

--- GameOfLife/GameOfLifeTests/Business/EdgeWrappingNeighbourFinderTests.cs
+++ GameOfLife/GameOfLifeTests/Business/EdgeWrappingNeighbourFinderTests.cs
@@ -18,46 +18,21 @@ namespace GameOfLifeTests.Business
             _edgeWrappingNeighbourFinder = new EdgeWrappingNeighbourFinder(aliveReadOnlyCenterCellGrid);
         }
 
+
+        [TestCase(0, 0, 1, 1)]
+        [TestCase(0, 2, 1, 2)]
+        [TestCase(2, 2, 2, 2)]
+        [TestCase(2, 0, 2, 1)]
+        [TestCase(1, 1, 0, 0)]
         
-        
-        [Test]
-        public void It_Should_Return_The_Active_Cell_In_The_Middle_Of_The_Neighbours_Grid_Given_The_Top_Left_Cell() {
-            var centerCell = new CellPosition(0, 0);
-            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(centerCell);
+        public void NeighbourFinder_Should_Find_Wrapped_Neighbours(int targetRow, int targetColumn, int expectedAliveCellRow, int expectedAliveCellColumn)
+        {
+            var targetCell = new CellPosition(targetRow, targetColumn);
             
-            Assert.AreEqual(CellState.Alive, neighbours.Grid[1][1].GetState());
-        }
-        
-        [Test]
-        public void It_Should_Return_The_Active_Cell_In_The_Middle_Right_Of_The_Neighbours_Grid_Given_The_Top_Right_Cell() {
-            var centerCell = new CellPosition(0, 2);
-            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(centerCell);
-            
-            Assert.AreEqual(CellState.Alive, neighbours.Grid[1][2].GetState());
-        }
-        
-        [Test]
-        public void It_Should_Return_The_Active_Cell_In_The_Bottom_Right_Of_The_Neighbours_Grid_Given_The_Bottom_Right_Cell() {
-            var centerCell = new CellPosition(2, 2);
-            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(centerCell);
-            
-            Assert.AreEqual(CellState.Alive, neighbours.Grid[2][2].GetState());
-        }
-        
-        [Test]
-        public void It_Should_Return_The_Active_Cell_In_The_Bottom_Center_Of_The_Neighbours_Grid_Given_The_Bottom_Left_Cell() {
-            var centerCell = new CellPosition(2, 0);
-            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(centerCell);
-            
-            Assert.AreEqual(CellState.Alive, neighbours.Grid[2][1].GetState());
-        }
-        
-        [Test]
-        public void It_Should_Return_The_Active_Cell_In_The_Top_Right_Of_The_Neighbours_Grid_Given_The_Center_Cell() {
-            var centerCell = new CellPosition(1, 1);
-            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(centerCell);
-            
-            Assert.AreEqual(CellState.Alive, neighbours.Grid[0][0].GetState());
+            var neighbours = _edgeWrappingNeighbourFinder.GetThreeByThreeGridAroundCell(targetCell);
+
+            var expectedAliveCell = neighbours.Grid[expectedAliveCellRow][expectedAliveCellColumn];
+            Assert.AreEqual(CellState.Alive, expectedAliveCell.GetState());
         }
     }
 }
