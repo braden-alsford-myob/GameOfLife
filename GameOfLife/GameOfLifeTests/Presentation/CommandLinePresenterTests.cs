2fd9e307c137869490576a997b4b18ca75b9712e
Braden Alsford <braden.alsford@myob.com>
2020-04-21 11:38:16 +1000

    Changes CLI presenter to call writer once per generation and updates endtoend tests and clipresenter tests to reflect these changes.

--- GameOfLife/GameOfLifeTests/Presentation/CommandLinePresenterTests.cs
+++ GameOfLife/GameOfLifeTests/Presentation/CommandLinePresenterTests.cs
@@ -11,60 +11,99 @@ namespace GameOfLifeTests.Presentation
     public class CommandLinePresenterTests
     {
         private Mock<IWriter> _spyWriter;
-        private ReadOnlyGrid _testGrid;
-        private int _generationCount;
+
+        private IPresenter _presenter;
+        private GenerationViewModel _testGenerationViewModel;
         
         [SetUp]
         public void Setup()
         {
             _spyWriter = new Mock<IWriter>();
-            var presenter = new CommandLinePresenter(_spyWriter.Object);
-
-            _testGrid = GridCreator.GetOnlyAliveCenterGrid();
-            _generationCount = 10;
-
-            var generationVm = new GenerationViewModel(_testGrid, _generationCount);
+            _presenter = new CommandLinePresenter(_spyWriter.Object);
             
-            presenter.Display(generationVm);
+            _testGenerationViewModel = new GenerationViewModel(GridCreator.GetOnlyAliveCenterGrid(), 0);
         }
         
         [Test]
         public void Display_Should_Call_Clear_Once()
         {
+            _presenter.Display(_testGenerationViewModel);
+            
             _spyWriter.Verify(w => w.Clear(), Times.Once());
         }
 
-        [Test, TestCaseSource(nameof(OutputStrings))]
-        public void WriteLine_Should_Get_Called_With_Correct_Output(string output, int expectedTimes)
+        [Test]
+        public void Display_Should_Call_Clear_Before_WriteLine()
         {
-            _spyWriter.Verify(w => w.WriteLine(output), Times.Exactly(expectedTimes));
+            var callOrder = 0;
+            
+            _spyWriter.Setup(x => x.Clear()).Callback(() => Assert.That(callOrder++, Is.EqualTo(0)));
+            _spyWriter.Setup(x => x.WriteLine(It.IsAny<string>())).Callback(() => Assert.That(callOrder++, Is.EqualTo(1)));
+            
+            _presenter.Display(_testGenerationViewModel);
         }
         
-        private static IEnumerable<TestCaseData> OutputStrings
+        [Test, TestCaseSource(nameof(GenerationViewModels))]
+        public void WriteLine_Should_Get_Called_With_Correct_Output(GenerationViewModel generation, string expected)
+        {
+            _presenter.Display(generation);
+            
+            _spyWriter.Verify(w => w.WriteLine(expected), Times.Once);
+        }
+        
+        
+        private static IEnumerable<TestCaseData> GenerationViewModels
         {
             get
             {
                 yield return new TestCaseData(
+                    new GenerationViewModel(GridCreator.GetOnlyAliveCenterGrid(), 10),
                     OutputConstants.RowEdge +
                     OutputConstants.DeadRepresentation +
                     OutputConstants.DeadRepresentation +
                     OutputConstants.DeadRepresentation +
-                    OutputConstants.RowEdge,
-                    2);                
-                
-                yield return new TestCaseData(
-                    OutputConstants.RowEdge + 
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.RowEdge +
                     OutputConstants.DeadRepresentation +
                     OutputConstants.AliveRepresentation +
                     OutputConstants.DeadRepresentation +
-                    OutputConstants.RowEdge,
-                    1);           
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.RowEdge +
+                    OutputConstants.DeadRepresentation +
+                    OutputConstants.DeadRepresentation +
+                    OutputConstants.DeadRepresentation +
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.NewLine +
+                    OutputConstants.GenerationCount +
+                    "10");
                 
                 yield return new TestCaseData(
-                    OutputConstants.NewLine + 
-                    OutputConstants.GenerationCount + 
-                    10,
-                    1);
+                    new GenerationViewModel(GridCreator.GetAllAliveGrid(), 98765),
+                    OutputConstants.RowEdge +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.RowEdge +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.RowEdge +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.AliveRepresentation +
+                    OutputConstants.RowEdge +
+                    OutputConstants.NewLine +
+                    OutputConstants.NewLine +
+                    OutputConstants.GenerationCount +
+                    "98765");
+                
             }
         }
     }
